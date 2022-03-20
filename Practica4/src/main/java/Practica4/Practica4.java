5fe83bba0c816c72860bc34a84cec73f23a6386d
delionsgate <54085182+delionsgate@users.noreply.github.com>
2022-03-20 15:51:01 -0600

    Implementación con menú
    Se agregó la clase Menu.java y se implementaron las funciones del sistema operativo con el menú.

--- Practica4/src/main/java/Practica4/Practica4.java
+++ Practica4/src/main/java/Practica4/Practica4.java
@@ -1,8 +1,11 @@
 package Practica4;
+import java.util.Scanner;
+import java.io.IOException;
 
 /**
  *
- * @author axel
+ * @author Nuñez Quintana, Luis Axel
+ * @author Zárate García, Zuriel
  */
 public class Practica4 {
 
@@ -12,103 +15,154 @@ public class Practica4 {
      */
     public static void main(String[] args) {
         SistemaOperativo SO = SistemaOperativo.getSistemaOperativo();
+       
+        System.out.println("Gracias por instalar...");
+        System.out.println("    __________________________________________________________________");
+        System.out.println("");
+        System.out.println("            ´\"_/_/´\"´\" _/_/_/_/_/´\" ´\"´\" ´\"_/_/´\"´\" ´\"_/_/_/´\" ");
+        System.out.println("            _/´\"´\"_/´\" ´\"´\"´\"_/´\"´\" ´\"´\" _/´\"´\"_/´\" _/´\"´\"´\"´\" ");
+        System.out.println("            _/_/_/_/´\" ´\"´\"_/´\"´\"´\" ´\"´\" _/´\"´\"_/´\" ´\"_/_/´\"´\" ");
+        System.out.println("            _/´\"´\"_/´\" ´\"_/´\"´\"´\"´\" ´\"´\" _/´\"´\"_/´\" ´\"´\"´\"_/´\" ");
+        System.out.println("            _/´\"´\"_/´\" _/_/_/_/_/´\" ´\"´\" ´\"_/_/´\"´\" _/_/_/´\"´\" ");
+        System.out.println("    __________________________________________________________________\n");
+       
+        String[] opciones = {
+            "Crear proceso",
+            "Ver proceso actual",
+            "Ejecutar proceso actual",
+            "Pasar al proceso siguiente",
+            "Matar proceso actual",
+            "Ver estado actual del sistema",
+            "Imprimir cola de procesos",
+            "Salir del programa"
+        };
+        
+        int eleccion = 0;
+        while(eleccion != 8){
+            switch(eleccion){
+                case 0:
+                    try{
+                        eleccion = Menu.menu(opciones);
+                    }catch(IOException e){
+                        System.out.println("Algo salio mal");
+                    }
+                    break;
+                
+                case 1:
+                    System.out.print("Ingresa el nombre del proceso: ");
+                    Scanner nombreP = new Scanner(System.in);
+                    String nombre = nombreP.nextLine();
+                    System.out.print("\n Elige el tama\u00f1o de tu proceso: ");
+                    String[] tamanios = {"64","128","256","512","Cancelar"};
+                    
+                    int opcase1 = 0;
+                    while(opcase1!=5){
+                        
+                        boolean disponible;
+                        
+                        switch(opcase1){
+                            case 0:
+                                try{
+                                    opcase1 = Menu.menu(tamanios);
+                                }catch(IOException e){
+                                    System.out.println("Algo salio mal");
+                                }
+                                break;
+                                
+                            case 1:
+                                disponible = SO.creaProceso(nombre, 64);
+                                if(disponible == true){
+                                    System.out.println("El proceso se ha creado con exito! Agregado a la cola :D");
+                                }
+                                else{
+                                    System.out.println("No hay espacio suficiente para el proceso :C");
+                                }
+                                opcase1 = 5;
+                                break;
+                                
+                            case 2:
+                                disponible = SO.creaProceso(nombre, 128);
+                                if(disponible == true){
+                                    System.out.println("El proceso se ha creado con exito! Agregado a la cola :D");
+                                }
+                                else{
+                                    System.out.println("No hay espacio suficiente para el proceso :C");
+                                }
+                                opcase1 = 5;
+                                break;
+                                
+                            case 3:
+                                disponible = SO.creaProceso(nombre, 256);
+                                if(disponible == true){
+                                    System.out.println("El proceso se ha creado con exito! Agregado a la cola :D");
+                                }
+                                else{
+                                    System.out.println("No hay espacio suficiente para el proceso :C");
+                                }
+                                opcase1 = 5;
+                                break;
+                                
+                            case 4:
+                                disponible = SO.creaProceso(nombre, 512);
+                                if(disponible == true){
+                                    System.out.println("El proceso se ha creado con exito! Agregado a la cola :D");
+                                }
+                                else{
+                                    System.out.println("No hay espacio suficiente para el proceso :C");
+                                }
+                                opcase1 = 5;
+                                break;
+                                
+                            case 5:
+                                opcase1 = 0;
+                                break;
+                        }
+                    }
+                    
+                    eleccion=0;
+                    break;
+                    
+                case 2:
+                    SO.verProcesoActual();
+                    eleccion=0;
+                    break;
+                    
+                case 3:
+                    System.out.println("Ejecutando proceso...");
+                    SO.ejecutaProceso();
+                    eleccion=0;
+                    break;
+                    
+                case 4:
+                    SO.siguienteProceso();
+                    eleccion=0;
+                    break;
+                    
+                case 5:
+                    System.out.println("Eliminando proceso actual...");
+                    SO.eliminaProceso();
+                    eleccion=0;
+                    break;
+                    
+                case 6:
+                    System.out.println("Estado del sistema: ");
+                    SO.verEstado();
+                    eleccion=0;
+                    break;
+                    
+                case 7:
+                    SO.verProcesos();
+                    eleccion=0;
+                    break;
+                    
+                case 8:
+                    System.out.println("Saliendo...");
+                    SO.salirSistema();
+                    eleccion=0;
+                    break;
+            }
 
-        System.out.println("############## 0 Procesos ##############");
-        //Operaciones al tener 0 procesos
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## COLA ##############");
-        SO.verProcesos();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-        System.out.println("############## EJECUTA ##############");
-        SO.ejecutaProceso();
-        System.out.println("############## ELIMINA ##############");
-        SO.eliminaProceso();
-        System.out.println("############## SIGUIENTE ##############");
-        SO.siguienteProceso();
-        System.out.println("############## SALIR ##############");
-        SO.salirSistema();
-
-        System.out.println("############################");
-        System.out.println("############## 1 Proceso ##############");
-        //Operaciones al tener 1 proceso
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word.exe", 512);
-
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## COLA ##############");
-        SO.verProcesos();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-        System.out.println("############## EJECUTA ##############");
-        SO.ejecutaProceso();
-        System.out.println("############## SIGUIENTE ##############");
-        SO.siguienteProceso();
-        System.out.println("############## SALIR ##############");
-        SO.salirSistema();
-
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-        System.out.println("############## ELIMINA ##############");
-        SO.eliminaProceso();
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-
-        System.out.println("############################");
-        System.out.println("############## Muchos Procesos ##############");
-        //Operaciones al tener max procesos
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word2.exe", 512);
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word3.exe", 512);
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word4.exe", 512);
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word5.exe", 512);
-        System.out.println("############## CREA ##############");
-        SO.creaProceso("Word6.exe", 512);
-
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## COLA ##############");
-        SO.verProcesos();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-        System.out.println("############## EJECUTA ##############");
-        SO.ejecutaProceso();
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## SIGUIENTE ##############");
-        SO.siguienteProceso();
-        System.out.println("############## SALIR ##############");
-        SO.salirSistema();
-
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-        System.out.println("############## ELIMINA ##############");
-        SO.eliminaProceso();
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
-
-        System.out.println("############## EJECUTA ##############");
-
-        for (int i = 0; i < 20; i++) {
-            SO.ejecutaProceso();
         }
-
-        System.out.println("############## ESTADO ##############");
-        SO.verEstado();
-        System.out.println("############## ACTUAL ##############");
-        SO.verProcesoActual();
+        
     }
 }
