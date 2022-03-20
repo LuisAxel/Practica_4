7297e658d11017109c8e37fb75c24b2c65937530
LuisAxel <54085467+LuisAxel@users.noreply.github.com>
2022-03-19 21:30:50 -0600

    Add files via upload

---
+++ Practica4/src/main/java/Practica4/Practica4.java
@@ -0,0 +1,12 @@
+package Practica4;
+
+/**
+ *
+ * @author axel
+ */
+public class Practica4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Practica4/src/main/java/Practica4/Proceso.java
@@ -0,0 +1,120 @@
+
+package Practica4;
+
+import java.util.Random;
+
+/**
+ * Clase Proceso, modela al objeto proceso. 
+ * 
+ * Cuenta con los atributos: 
+ * 
+ * |> nombre: nombre del proceso
+ *
+ * @author Núñez Quintana, Luis Axel
+ * @author Zárate García, Zuriel
+ *
+ */
+public class Proceso {
+    private String nombre;
+    private int id;
+    private int[] instrucciones; // 2 [totales, ejecutadas]
+    private int[] direcciones_memoria;// 2 [base, limite]
+    private String estado;
+    private static int num_procesos;
+    
+    public Proceso(String nombre, int dir_base, int dir_limite){
+        id = ++num_procesos;
+        setNombre(nombre);
+        direcciones_memoria = new int[2];
+        setDir(dir_base, dir_limite);
+        instrucciones = new int[2];
+        setInstrucciones();
+        setEstado(0);
+    }
+    
+    public void setNombre(String nombre) {
+        if(nombre.length() > 2 && nombre.length() < 50){
+            this.nombre = nombre;
+        }else{
+            System.out.println("El nombre debe tener al menos 3 caracteres y menos de 50");
+            this.nombre = null;
+        }            
+    }
+    
+    public void setDir(int dir_base, int dir_limite) {
+        this.direcciones_memoria[0] = dir_base;
+        this.direcciones_memoria[1] = dir_limite;
+    }
+    
+    public void setInstrucciones() {
+        Random rand = new Random();
+        this.instrucciones[0] = 0;
+        this.instrucciones[1] = rand.nextInt(21) + 10;
+    }
+    
+    public void setEstado(int estado) {
+        switch(estado){
+            case 0:
+                this.estado = "Nuevo";
+                break;
+            
+            case 1:
+                this.estado = "En ejecucion";
+                break;
+            
+            case 2:
+                this.estado = "Preparado";
+                break;
+                
+            case 3:
+                this.estado = "Terminado";
+                break;
+        }
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+    
+    public int[] getInstrucciones() {
+        return instrucciones;
+    }
+    
+    public int[] getDirecciones() {
+        return direcciones_memoria;
+    }
+    
+    public boolean realizaInstrucciones() {
+        this.instrucciones[1] -= 5;
+        if(this.instrucciones[1] <= 0){
+            System.out.println("\tEl proceso ha concluido su ejecucion");
+            this.instrucciones[1] = 0;
+            this.setEstado(3);
+            return false;
+        }
+        this.setEstado(2);
+        return true;
+    }
+    
+    public void eliminaProceso() {
+        this.setEstado(3);
+    }
+    
+    public void imprimeProceso(){
+        System.out.println("\tNombre del Proceso: " + getNombre());
+        System.out.println("\tId del Proceso: " + getId());
+        System.out.println("\tEstado del Proceso: " + getEstado());
+        System.out.println("\tInstrucciones totales del Proceso: " + getInstrucciones()[0]);
+        System.out.println("\tInstrucciones ejecutadas del Proceso: " + getInstrucciones()[1]);
+        System.out.println("\tDireccion base del Proceso: " + getDirecciones()[0]);
+        System.out.println("\tDireccion limite del Proceso: " + getDirecciones()[1]);
+    }
+}
